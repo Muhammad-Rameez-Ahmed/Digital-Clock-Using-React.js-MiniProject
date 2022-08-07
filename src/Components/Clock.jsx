c64b6e1e6435da4b42a5eb07914395249ea35f04
Rameez <muhammadrameezahmed73@gmail.com>
2022-08-07 19:36:09 +0500

    Clock Component show clock

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 const Clock = () => {
+    let todaytime = new Date().toLocaleTimeString();
+    const [time, setTime] = useState(todaytime);
+
+    const updateTime=()=>{
+        let todaytime = new Date().toLocaleTimeString();
+        setTime(todaytime);
+        
+
+    }
+  
+setInterval(updateTime,1000);
+
+
     return (
         <>
-            <h1>clock here</h1>
+            <h1>Digital Clock Mini-Project</h1>
+            <h2>{time}</h2>
         </>
 
     )
